5800eaf67b2212c449dc40be495423f16a0095ed
Maksim Slyunkov <slyunkovmax@gmail.com>
2020-05-17 19:01:05 +0300

    first version
    first version of project 12

---
+++ routes/cards.js
@@ -0,0 +1,17 @@
+const cardsRouter = require('express').Router();
+const fsPromises = require('fs').promises;
+const path = require('path');
+const cardsPath = path.join('data', 'cards.json');
+
+cardsRouter.get('/cards', (req, res) => {
+  fsPromises.readFile(cardsPath, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(JSON.parse(data));
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send(err.message);
+    });
+});
+
+module.exports = cardsRouter;
---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const usersRouter = require('express').Router();
+const fsPromises = require('fs').promises;
+const path = require('path');
+const usersPath = path.join('data', 'users.json');
+
+usersRouter.get('/users', (req, res) => {
+  fsPromises.readFile(usersPath, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(JSON.parse(data));
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send(err.message);
+    });
+});
+
+usersRouter.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  fsPromises.readFile(usersPath, { encoding: 'utf8' })
+    .then((data) => {
+      if (!(JSON.parse(data).some((item) => item._id === id))) {
+        res.send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
+      res.send(JSON.parse(data).find((item) => item._id === id));
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send(err.message);
+    });
+});
+
+module.exports = usersRouter;
